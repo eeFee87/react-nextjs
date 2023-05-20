c72610f3ef460cadf0d73db3cdcc268d43617594
efe <eefee1987@gmail.com>
2023-05-20 19:16:25 +0200

    semicolon borrado

--- app/posts/[id]/layout.jsx
+++ app/posts/[id]/layout.jsx
@@ -1,14 +1,14 @@
-import Link from 'next/link';
+import Link from 'next/link'
 
 const fetchSinglePost = (id) => {
   return fetch(`https://jsonplaceholder.typicode.com/posts/${id}`).then((res) =>
     res.json()
-  );
-};
+  )
+}
 
 export default async function Post({ children, params }) {
-  const { id } = params;
-  const post = await fetchSinglePost(id);
+  const { id } = params
+  const post = await fetchSinglePost(id)
   return (
     <article>
       <h1 className="text-3xl py-4 text-blue-400 p-4 ">{post.title}</h1>
@@ -17,5 +17,5 @@ export default async function Post({ children, params }) {
       </Link>
       {children}
     </article>
-  );
+  )
 }
